1d5398c512e1f79c421d0618f77a94dbc355e45e
Taichi22 <bcong@emich.edu>
2024-02-12 10:34:44 -0500

    2nd commit

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,5 +3,6 @@ package eclipseSoloProjA;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Java Project!");
+		System.out.println("Adding changes");
 	}
 }
