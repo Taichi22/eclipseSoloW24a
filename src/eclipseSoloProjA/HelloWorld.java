300260a25a778cebde654b3cfae1a829208dd6c0
bcong <bcong@emich.edu>
2024-02-12 10:43:54 -0500

    merge commit

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,6 +3,6 @@ package eclipseSoloProjA;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Java Project!");
-		System.out.println("Adding changes");
+		System.out.println("Adding changes to create merge conflict");
 	}
 }
